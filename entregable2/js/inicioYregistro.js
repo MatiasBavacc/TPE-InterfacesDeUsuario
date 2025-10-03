01b1be4f4ef189665da10c9b8f6090d662a99aab
MatiasBavaCc <matiasbavacc@gmail.com>
2025-10-03 19:17:26 -0300

    actualizando el envio de los fromularios con inicioYregistro.js

--- entregable2/js/inicioYregistro.js
+++ entregable2/js/inicioYregistro.js
@@ -1,9 +1,22 @@
 const passwordInput = document.getElementById('contrasena-campo');
 const toggleImg = document.getElementById('togglePassword');
 
+const registro = document.querySelector('.registro-form');
+const login = document.querySelector('.login-form');
+
 toggleImg.addEventListener('click', () => {
   const isPassword = passwordInput.type === 'password';
   passwordInput.type = isPassword ? 'text' : 'password';
   // Opcional: Cambia la imagen según el estado
   toggleImg.src = isPassword ? 'img/Vector.png' : 'img/basil_eye-closed-solid.png';
 });
+
+
+
+registro.addEventListener('submit', prevenirEnvio);
+login.addEventListener('submit', prevenirEnvio);
+
+function prevenirEnvio(event) {
+  event.preventDefault();
+  window.location.href = 'index.html';
+}
