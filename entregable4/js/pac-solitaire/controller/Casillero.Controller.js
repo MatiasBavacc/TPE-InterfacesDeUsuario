eac4e66587d06a08c545bb96634c519006ce7305
Joaquin <joaquinf2104@gmail.com>
2025-10-31 20:54:32 -0300

    cambios en casilla

--- entregable4/js/pac-solitaire/controller/Casillero.Controller.js
+++ entregable4/js/pac-solitaire/controller/Casillero.Controller.js
@@ -2,9 +2,54 @@ import { CasilleroModel } from "../model/Casillero.Model.js";
 import { CasilleroView } from "../view/Casillero.View.js";
 
 export class CasilleroController {
-      constructor(){
+      constructor(x, y, ancho, alto, ficha, ctx){
             this.model = new CasilleroModel();
             this.view = new CasilleroView();
+            this.ficha = ficha;
+            this.ctx = ctx;
+            this.x = x;
+            this.y = y;
+            this.ancho = ancho;
+            this.alto = alto;
       }
+
+      dibujarCasilla(ctx, color = "", ficha) {
+        ctx.beginPath();
+        ctx.ellipse(
+        this.x + this.ancho / 2,  // centro X
+        this.y + this.alto / 2,   // centro Y
+        this.ancho / 2,           // radioX
+        this.alto / 2,            // radioY
+        0,                        // rotación
+        0,                        // ángulo inicio
+        2 * Math.PI               // ángulo fin
+        );
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.stroke();
+
+        if (ficha != null) {
+            this.ficha.dibujarFicha(this.ctx, this.x, this.y);
+        }
+        }
+
+      marcarCasilla(ctx) {
+                  this.dibujarCasilla(ctx, "green");
+      }
+
+      estaDentro(x, y) {
+        return (
+        x >= this.x &&
+        x <= this.x + this.ancho &&
+        y >= this.y &&
+        y <= this.y + this.alto
+        );
+    }
+
+    eliminarficha(ctx){
+      this.ficha = null;
+      this.dibujarCasilla(ctx);
+    }
+
 }
 export default CasilleroController;
